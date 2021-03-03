8f84d960d5388689cdbb922ae8294589ff034ce2
WHOOC <info@whooc.com>
2021-03-03 17:28:09 +0800

    测试

--- conf.js
+++ conf.js
@@ -1,99 +1,85 @@
-jsproxy_config({
-  // 当前配置的版本（记录在日志中，用于排查问题）
-  // 每次修改配置，该值需要增加，否则不会生效。
-  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '107',
+jsproxy_config （{
+  //当前配置的版本（记录在日志中，用于排查问题）
+  //每次修改配置，该值需要增加，否则不会生效。
+  //默认每隔5分钟自动下载配置，若想立即验证，可通过任意模式访问。
+  ver：'107' ，
 
-  // 通过 CDN 加速常用网站的静态资源（实验中）
-  static_boost: {
-    enable: true,
-    ver: 60
-  },
+  //通过CDN加速常用网站的静态资源（实验中）
+  static_boost：{
+    启用：true ，
+    版本：60
+  } ，
 
-  // 节点配置
-  // node_map: {
-  //   'demo-hk': {
-  //     label: '演示服务-香港节点',
-  //     lines: {
-  //       // 主机:权重
-  //       'node-aliyun-hk-0.etherdream.com:8443': 1,
-  //       'node-aliyun-hk-1.etherdream.com:8443': 1,
-  //       'node-aliyun-hk-2.etherdream.com:8443': 1,
-  //     }
-  //   },
-   //  'demo-sg': {
-   //    label: '演示服务-新加坡节点',
-   //    lines: {
-   //      'node-aliyun-sg.etherdream.com:8443': 1,
-   //    },
-  //  },
-  //   'mysite': {
-  //     label: '当前站点',
-  //     lines: {
-  //       [location.host]: 1,
-   //    }
-   //  },
-    // 该节点用于加载大体积的静态资源
-    'cfworker': {
-      label: '',
-      hidden: true,
-      lines: {
-        // 收费版（高权重）
-        'node-cfworker.etherdream.com': 4,
+  //官方配置
+  node_map：{
+    'demo-hk'：{
+      标签：“演示服务-香港例程” ，
+      行：{
+        //主机：权重
+        'node-aliyun-hk-0.etherdream.com:8443' ：1 ，
+        'node-aliyun-hk-1.etherdream.com:8443' ：1 ，
+        'node-aliyun-hk-2.etherdream.com:8443' ：1 ，
+      }
+    } ，
+    'demo-sg'：{
+      标签：“演示服务-新加坡中断” ，
+      行：{
+        'node-aliyun-sg.etherdream.com:8443' ：1 ，
+      } ，
+    } ，
+    'mysite'：{
+      标签：“当前站点” ，
+      行：{
+        [位置。主持人]：1 ，
+      }
+    } ，
+    //该股东用于加载大体积的静态资源
+    'cfworker'：{
+      标签：'' ，
+      隐藏的：true ，
+      行：{
+        //收费版（高权重）
+        'node-cfworker.etherdream.com'：4 ，
 
-        // 免费版（低权重，分摊一些成本）
-        // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
-        'b.hehe.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
+        //免费版（低权重，分摊一些成本）
+        //每个账号每天10万次免费请求，但有频率限制
+        'b.007.workers.dev'：1 ，
+        'b.hehe.workers.dev'：1 ，
+        'b.lulu.workers.dev'：1 ，
+        'b.jsproxy.workers.dev'：1 ，
       }
     }
-  },
+  } ，
 
-  /**
-   * 默认节点
-   */
-  // node_default: 'mysite',
-  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  / **
+   *默认基线
+   * /
+  // node_default：'mysite'，
+  node_default：/ jsproxy-demo \。\ w + $ / 。测试（地点，主持人）？'demo-hk'：'mysite' ，
 
-  /**
-   * 加速节点
-   */
-  node_acc: 'cfworker',
+  / **
+   *加速推理
+   * /
+  node_acc：'cfworker' ，
 
-  /**
-   * 静态资源 CDN 地址
-   * 用于加速 `assets` 目录中的资源访问
-   */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  / **
+   *静态资源CDN地址
+   *用于加速`assets`目录中的资源访问
+   * /
+  asset_cdn：'https : //cdn.jsdelivr.net/gh//whooc/wh123.github.io@master/assets/ ' ，
+  //本地测试时打开，否则访问的是线上的
+  // asset_cdn：'assets /'，
 
-  // 本地测试时打开，否则访问的是线上的
-  // assets_cdn: 'assets/',
+  //首页路径
+  index_path：'index_v3.html' ，
 
-  // 首页路径
-  index_path: 'index_v3.html',
+  //支持CORS的站点列表（实验中...）
+  direct_host_list：'cors_v1.txt' ，
 
-  // 支持 CORS 的站点列表（实验中...）
-  direct_host_list: 'cors_v1.txt',
+  / **
+   *自定义注入页面的HTML
+   * /
+  inject_html：'<！-自定义html- >' ，
 
-  /**
-   * 自定义注入页面的 HTML
-   */
-  inject_html: '<!-- custom html -->',
 
-  /**
-   * URL 自定义处理（设计中）
-   */
-  // url_handler: {
-  //   'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-  //     replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-  //   },
-  //   'https://www.pornhub.com/': {
-  //     redir: 'https://php.net/'
-  //   },
-  //   'http://haha.com/': {
-  //     content: 'Hello World'
-  //   },
-  // }
-})
+} ）
