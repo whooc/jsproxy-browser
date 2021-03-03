d2c43e91d672b493c0ec9f900e54df60411498ea
WHOOC <info@whooc.com>
2021-03-03 17:18:24 +0800

    测试
    测试

--- conf.js
+++ conf.js
@@ -11,28 +11,28 @@ jsproxy_config({
   },
 
   // 节点配置
-  node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
-    'mysite': {
-      label: '当前站点',
-      lines: {
-        [location.host]: 1,
-      }
-    },
+  // node_map: {
+  //   'demo-hk': {
+  //     label: '演示服务-香港节点',
+  //     lines: {
+  //       // 主机:权重
+  //       'node-aliyun-hk-0.etherdream.com:8443': 1,
+  //       'node-aliyun-hk-1.etherdream.com:8443': 1,
+  //       'node-aliyun-hk-2.etherdream.com:8443': 1,
+  //     }
+  //   },
+   //  'demo-sg': {
+   //    label: '演示服务-新加坡节点',
+   //    lines: {
+   //      'node-aliyun-sg.etherdream.com:8443': 1,
+   //    },
+  //  },
+  //   'mysite': {
+  //     label: '当前站点',
+  //     lines: {
+  //       [location.host]: 1,
+   //    }
+   //  },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: '',
@@ -85,15 +85,15 @@ jsproxy_config({
   /**
    * URL 自定义处理（设计中）
    */
-  url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
-    'http://haha.com/': {
-      content: 'Hello World'
-    },
-  }
+  // url_handler: {
+  //   'https://www.baidu.com/img/baidu_resultlogo@2.png': {
+  //     replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
+  //   },
+  //   'https://www.pornhub.com/': {
+  //     redir: 'https://php.net/'
+  //   },
+  //   'http://haha.com/': {
+  //     content: 'Hello World'
+  //   },
+  // }
 })
